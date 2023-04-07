27490a698b42c4e269881218a62044e2bcb1d3bd
blender2 <94746805+blender2@users.noreply.github.com>
2023-04-07 22:23:54 +0300

    try1

--- solution.py
+++ solution.py
@@ -4,14 +4,18 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 469912664 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = x * 2/t**2
+    n = np.shape(x)[0]
+    avg = 0.5
+    x = avg - x
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+
+    scale = x.min()*110
+    return avg + norm.ppf(1 - alpha / 2)/n/scale, \
+           avg + norm.ppf(alpha / 2)/n/scale
